608e69c7335270a894a03944d89264fa10e6ac53
techybibi <51873081+techybibi@users.noreply.github.com>
2020-07-18 15:00:14 +0530

    process test

---
+++ process_test.php
@@ -0,0 +1,17 @@
+<?php
+// include("mpdf/mpdf.php");
+
+// $mpdf = new mPDF();
+// $mpdf->WriteHTML('<h1>Hello world!</h1>');
+// $mpdf->Output()
+
+//$firstName = $_POST['firstName'];
+//echo $firstName;
+//workspace
+
+//$photoapplicant =$_POST['filephoto'];
+
+
+
+
+?>
